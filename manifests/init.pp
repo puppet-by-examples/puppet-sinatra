bbf361d16fd0086d6559d731e5a3061bd2f756da
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-10-20 13:57:47 +0200

    Updated paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,14 +2,12 @@ class sinatra {
 
     exec { 'sinatra:install':
         command => "gem install sinatra",
-        path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-        require => Class['php5']
+        path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
     }
 
     exec { 'sinatra:thin':
         command => "gem install thin",
-        path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-        require => Class['php5']
+        path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
     }
 
 

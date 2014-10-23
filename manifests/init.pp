144ab235a18c33a855310ed534783723c2c032e7
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-10-23 07:45:32 +0200

    Timeout 6000

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class sinatra {
 
     exec { 'sinatra:install':
         command => "gem install sinatra",
+        timeout => 6000,
         path    => '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin'
     }
 
